89dc84edc8a4039bbc43d92c8b66904644179331
ShutenkoStas <shotenko494@gmail.com>
2025-12-24 00:28:23 +0200

    Add files for templates also add 2 views

--- note_app/views.py
+++ note_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from note_app import models
 
-# Create your views here.
+class NoteListView(ListView):
+    model = models.Note
+    context_object_name = "list_notes"
+    template_name = "note_list.html"
+
+class NoteDetailView(DetailView):
+    model = models.Note
+    context_object_name = "detail_notes"
+    template_name = "note_detail.html"
+
